b6ae5db1b3bb9671789fb6c5e7dea5c578eee053
mhscio <mhscio>
2019-11-18 15:38:36 +0100

    Initial Commit

---
+++ APP-SCONTRINI/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  APP-SCONTRINI
+//
+//  Created by Mhscio on 18/11/2019.
+//  Copyright © 2019 Mhscio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ APP-SCONTRINI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  APP-SCONTRINI
+//
+//  Created by Mhscio on 18/11/2019.
+//  Copyright © 2019 Mhscio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
